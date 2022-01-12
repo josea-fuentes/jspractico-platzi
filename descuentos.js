8340c2ae49926bc26515d15fc5b0160202858b1b
jose <jose.fuentes.0107@gmail.com>
2022-01-12 18:00:09 -0500

    update

--- descuentos.js
+++ descuentos.js
@@ -3,3 +3,16 @@ function calcularPrecioConDescuento(precio,descuento) {
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
 }
+
+function ButtonPriceDiscount() {
+    const inputPrecio = document.getElementById("InputPrecio");
+    const precioValue = inputPrecio.value;
+
+    const inputDescuento = document.getElementById("InputDescuento")
+    const descuentoValue = inputDescuento.value; 
+
+    const precioConDesceunto = calcularPrecioConDescuento(precioValue,descuentoValue);
+
+    const resultadoPrecio = document.getElementById("ResultadoPrecio");
+    resultadoPrecio.innerText = "El precio con descuento son: $" + precioConDesceunto; 
+}
